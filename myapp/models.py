0ad731962c50eb8bd8a9a4bed851e947270be512
wajahat ahmed <wajahat.ahmed245@gmail.com>
2018-05-31 10:42:36 -0700

    debuging and model sol

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,4 @@
+from django.db import connection
 from django.db import models
 
 # Create your models here.
@@ -8,3 +9,22 @@ class Student(models.Model):
     contact    = models.IntegerField()  
     email      = models.EmailField(max_length=50)  
     age        = models.IntegerField()  
+
+
+class Employee(models.Model):  
+    eid      = models.CharField(max_length=20)  
+    ename    = models.CharField(max_length=100)  
+    econtact = models.CharField(max_length=15)  
+    class Meta:  
+        db_table = "employee"       
+
+
+
+
+
+def mysql():
+    with connection.cursor() as cursor:
+        cursor.execute("select * from company ")
+        row = cursor.fetchall()
+
+    return row
--- myapp/views.py
+++ myapp/views.py
@@ -2,25 +2,39 @@ from django.shortcuts import render
 #placing template lib 
 from django.template import loader  
 # Create your views here.
-from django.http import HttpResponse  
-
+from django.http import HttpResponse 
+from django.db import connection
+from myapp.models import mysql
+# import pdb; pdb.set_trace() #for testing 
 titles={
-        'company':'Stashing words'
-    }
-    
+            'company':'Stashing words'
+            
+          
+        }
+
+
 def hello(request):  
     return HttpResponse("<h2>Hello, Welcome to Django!</h2>")  
 
 
 
 def index(request):
+    T=mysql()
+   
+    
     template=loader.get_template('index.html')  
    
-    return HttpResponse(template.render(titles))  
+    return HttpResponse(template.render())  
 
 
 
 def signup(request):
+    if 'First-Name' in request.GET and request.GET['First-Name']:
+       p1 = request.GET['First-Name']
+
+    
     template=loader.get_template('signup.html')  
     return HttpResponse(template.render(titles))  
     
+
+
